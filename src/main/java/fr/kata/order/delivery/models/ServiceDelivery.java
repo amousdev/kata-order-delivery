7ff84a97ae6ec6d8f868ba2a524ab1d0e8186a79
Amine MO <info@info-dev.fr>
2024-05-29 14:00:42 +0200

    feat(Order Delivery) : ajout des models delivery : story1

---
+++ src/main/java/fr/kata/order/delivery/models/ServiceDelivery.java
@@ -0,0 +1,14 @@
+package fr.kata.order.delivery.models;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ServiceDelivery {
+
+    private Long idService;
+    private Long idStore;
+    private DeliveryMethodEnum deliveryMethod;
+    private boolean enable;
+}
